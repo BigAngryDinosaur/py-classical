fa8048ee89946d8fe324ff27773e96e8853d731f
Hrishikesh Sawant <hrishikesh.sawant322@gmail.com>
2020-09-11 01:46:31 +0800

    1.1 Fib

---
+++ book/fib.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from typing import Generator
+
+@lru_cache(maxsize=None)
+def fib(n):
+   if n < 2:
+       return n
+   return fib(n-1) + fib(n-1)
+
+
+adsdfa
+def fib_gen(n: int) -> Generator[int, None, None]:
+    yield 0
+    if n > 1: yield 1
+    prev: int = 0
+    cur: int  = 1
+    for _ in range(1, n):
+        prev, cur = cur, prev + cur
+        yield cur
---
+++ book/main.py
@@ -0,0 +1,4 @@
+from .fib import *
+
+def start():
+    print([x for x in fib_gen(10)])
