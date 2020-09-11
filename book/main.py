a055f65e8d9e50b334139115b0ffff8b00fb7c79
Hrishikesh Sawant <hrishikesh.sawant322@gmail.com>
2020-09-11 14:21:38 +0800

    Add Nucleotide compressor

--- book/main.py
+++ book/main.py
@@ -1,4 +1,13 @@
 from .fib import *
+from .nucleotide_compressor import NucleotideCompressor
 
 def start():
-    print([x for x in fib_gen(10)])
+    #print([x for x in fib_gen(10)])
+    compress_gene()
+
+
+def compress_gene():
+    gene = "CCGTTATATATATATAGCCATGGATCGATTATA"
+    compressor = NucleotideCompressor(gene)
+    print(compressor.bit_string)
+    print(compressor)
